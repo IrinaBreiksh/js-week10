0a2572c8372e70b5a5dcef92b14af34fb9a472cc
Irina <irinabreiksh@gmail.com>
2023-10-18 11:15:44 +0300

    создала разные функции, использовала onclick и alert

---
+++ week10.task2.js
@@ -0,0 +1,25 @@
+function postMessage(){
+    console.log('Я учу JavaScript!');
+}
+postMessage();
+
+
+const image = document.getElementById('main__img');
+
+function changePhoto(){
+    image.src="./assets/images/cat-grey.jpg";
+}
+
+function changeNextPhoto(){
+    image.src="./assets/images/cat-white.jpg"
+}
+
+
+let quantity;
+let price;
+function calculateTotalPrice(quantity=2,price=15000000){
+    const result = quantity*price;
+    return alert(`Стоимость покупки ${result.toLocaleString("ru-RU")} рублей`);
+}
+
+
